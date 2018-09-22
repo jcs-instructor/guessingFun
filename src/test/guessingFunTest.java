da77bfd9f397b6cc100cc5a3ec4f634aef3221e2
jcs-instructor <jcsilberman@gmail.com>
2018-09-21 20:30:30 -0400

    Set up template with first failing test.

---
+++ src/test/guessingFunTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class guessingFunTest {
+
+    @Test
+    public void firstTest() {
+        assertTrue(false);
+    }
+}
